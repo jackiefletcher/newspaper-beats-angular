ffb51b444119a85fb6a0af50d6ab366bb5023e8c
Jackie Fletcher <jaxskriv@gmail.com>
2015-02-27 10:05:03 -0800

    initialize files

---
+++ app.js
@@ -0,0 +1,9 @@
+var newspaperBeats = angular.module('newspaperBeats', ['ui.router']);
+
+newspaperBeats.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html",
+    controller: 'BeatsCtrl'
+  });
+});
